83151b3cb7d097b3bc87f20fcaeb64d0e42e7fbe
Mical29 <randomlyh2020.mm@gmail.com>
2020-10-18 07:35:31 -0400

    ....

--- hecommerce/core/views.py
+++ hecommerce/core/views.py
@@ -26,28 +26,6 @@ class HomeView(ListView):
             'ordered_count' : ordered_count,
             'newsletter'  : newsletter,
             'newsletters'  : newsletters
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            
-=======
->>>>>>> 2215bcb54ef95ea14bc94a95daa0dc79a3d3813b
-=======
->>>>>>> 2215bcb54ef95ea14bc94a95daa0dc79a3d3813b
-=======
->>>>>>> 2215bcb54ef95ea14bc94a95daa0dc79a3d3813b
-=======
->>>>>>> 2215bcb54ef95ea14bc94a95daa0dc79a3d3813b
-=======
->>>>>>> 2215bcb54ef95ea14bc94a95daa0dc79a3d3813b
-=======
->>>>>>> b0f10ad1b4cb4be4c81231dce9c528b89bd7fc8e
-=======
->>>>>>> b0f10ad1b4cb4be4c81231dce9c528b89bd7fc8e
         }
         return render(self.request,'product_lists.html',context)
 
