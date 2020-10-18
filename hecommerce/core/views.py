eee8fe7e8ef6a84fc96d6d7b6013e3ef3df1ce02
Mical29 <randomlyh2020.mm@gmail.com>
2020-10-18 07:29:44 -0400

    ....

--- hecommerce/core/views.py
+++ hecommerce/core/views.py
@@ -30,7 +30,10 @@ class HomeView(ListView):
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             
+=======
+>>>>>>> 2215bcb54ef95ea14bc94a95daa0dc79a3d3813b
 =======
 >>>>>>> 2215bcb54ef95ea14bc94a95daa0dc79a3d3813b
 =======
