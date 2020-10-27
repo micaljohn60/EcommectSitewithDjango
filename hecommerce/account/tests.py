4265876aaaa7b2df54243ace946871cb8b0d6bc2
micaljohn60 <micaljohn60@gmail.com>
2020-10-27 23:40:57 +0630

    add URL Test to account and core

--- hecommerce/account/tests.py
+++ hecommerce/account/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase,SimpleTestCase
+from django.urls import reverse,resolve
+from account.views import login_view,signup_view,VerificationView,logout_view
 
 # Create your tests here.
+
+class UrlTest(SimpleTestCase):
+    
+    def test_signup(self):
+        url = reverse('account:sign_up')
+        self.assertEquals(resolve(url).func,signup_view) 
+    
+    def test_login(self):
+        url = reverse('account:log_in')
+        self.assertEquals(resolve(url).func,login_view)   
+        
+    # def test_Verification(self):
+    #     url = reverse('account:activate', args=['(?P<uidb64>[^/]+)','/(?P<token>[^/]+)$'])
+    #     self.assertEquals(resolve(url).func.view_class,VerificationView)
+        
+    def test_logout(self):
+        url = reverse('account:log_out')
+        self.assertEquals(resolve(url).func,logout_view)
--- hecommerce/core/tests.py
+++ hecommerce/core/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import SimpleTestCase,TestCase,Client,RequestFactory
+from core.views import HomeView,ProductDetailView,Order_Summary,add_to_cart,remove_from_cart,Order_Summary
+from django.urls import reverse,resolve
+from core.models import Item
+from django.contrib.auth.models import AnonymousUser
 
 # Create your tests here.
+# Test Urls 
+class TestUrls(SimpleTestCase):
+    def test_core_HomeView_url(self):
+        url = reverse('core:productlists')
+        self.assertEquals(resolve(url).func.view_class,HomeView)
+    
+    def test_core_productDetailView_url(self):
+        url = reverse('core:productdetail', args=['some-slug'])
+        self.assertEquals(resolve(url).func.view_class,ProductDetailView)
+        
+    def test_core_Order_Summary_url(self):
+        url = reverse('core:order-summary')
+        self.assertEquals(resolve(url).func.view_class,Order_Summary)  
+        
+    def test_core_add_to_cart_url(self):
+        url = reverse('core:add-to-cart', args=['some-args'])
+        self.assertEquals(resolve(url).func,add_to_cart)
+          
+    def test_core_remove_from_cart(self):
+        url = reverse('core:remove-from-cart', args=['some-args'])
+        self.assertEquals(resolve(url).func,remove_from_cart)
+
+    def test_core_order_summary(self):
+        url = reverse('core:order-summary')
+        self.assertEquals(resolve(url).func.view_class,Order_Summary)
+        
+    
+        
+        
