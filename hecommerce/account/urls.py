cb6cd709cd5daa5fd126d14a4cb87115931d6c05
micaljohn60 <micaljohn60@gmail.com>
2020-11-01 22:19:36 +0630

    add sup-login, add some inline style

--- hecommerce/account/urls.py
+++ hecommerce/account/urls.py
@@ -3,7 +3,8 @@ from.views import (
     signup_view,
     login_view,
     logout_view,
-    VerificationView
+    VerificationView,
+    superuser_login_view
     
 )
 
@@ -11,6 +12,7 @@ app_name = "account"
 
 urlpatterns = [
     path('login/',login_view,name="log_in"),
+    path('admin-login',superuser_login_view,name="superuserlogin"),
     path('signup/',signup_view,name="sign_up"),
     path('logout/',logout_view,name="log_out"),
     path('activate/<uidb64>/<token>',VerificationView.as_view(),name="activate")
